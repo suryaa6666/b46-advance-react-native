0b6a25b134cace28f49789fa08ae4db0864924ef
suryaa6666 <suryaelidanto@gmail.com>
2023-06-07 15:15:12 +0700

    2.stack-navigation

--- Container.js
+++ Container.js
@@ -1,12 +1,39 @@
-import { View, useTheme, Text } from 'native-base'
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Form from "./src/screens/formNativeBase";
+import Hello from "./src/screens/hello";
+import IncDec from "./src/screens/incDec";
+import "react-native-gesture-handler";
 
 function Container() {
-    const theme = useTheme()
+  const Stack = createStackNavigator();
+
   return (
-    <View>
-      <Text color={"surya.200"} fontSize={100} fontFamily={"body"}>Bismillah</Text>
-    </View>
-  )
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={Hello}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen name="Form" component={Form} />
+        <Stack.Screen
+          name="IncDec"
+          component={IncDec}
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
-export default Container
+// <View>
+//   <Text color={"surya.200"} fontSize={100} fontFamily={"body"}>
+//     Bismillah
+//   </Text>
+// </View>
+export default Container;
